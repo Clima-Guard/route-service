21f88bb87bcf792463201417d5fdd93ade2266ae
daria-georgiana505 <geo505georgiana@gmail.com>
2025-03-24 00:43:29 +0200

    Added interpolation and polyline segment class

---
+++ RouteService/src/RouteService/Models/PolylineSegment.cs
@@ -0,0 +1,23 @@
+﻿using GeographicLib;
+using PolylinerNet;
+
+namespace RouteService.Models;
+
+public class PolylineSegment
+{
+    public PolylinePoint A { get; private set; }
+    public PolylinePoint B { get; private set; }
+
+    public PolylineSegment(PolylinePoint a, PolylinePoint b)
+    {
+        A = a;
+        B = b;
+    }
+    
+    public InverseGeodesicResult GetInverseGeodesicResult() => Geodesic.WGS84.Inverse(A.Latitude, A.Longitude, B.Latitude, B.Longitude); 
+
+    public double GetLength()
+    {
+        return GetInverseGeodesicResult().Distance;
+    }
+}
---
+++ RouteService/src/RouteService/Services/IInterpolationService.cs
@@ -0,0 +1,11 @@
+﻿using PolylinerNet;
+
+namespace RouteService.Services;
+
+public interface IInterpolationService
+{
+    IList<PolylinePoint> GetInterpolatedPointsAlongRouteAtGivenDistance(
+        IList<PolylinePoint> pointsAlongRoute, 
+        double routeDistance,
+        double distanceBetweenPoints);
+}
